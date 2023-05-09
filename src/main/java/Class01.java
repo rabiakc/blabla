d200ac59da2ee1d88c8b670c9ef7361ab8b02a00
Rabia Koç <rabiakkcc@gmail.com>
2023-05-09 15:30:10 +0300

    V3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,7 +1,8 @@
 public class Class01 {
     public static void main(String[] args) {
         //V1 çalışmalarım...
-        //V2 kodları
+        //V2 kodları..
+        //V3 çalışmalar..
 
     }
 }
