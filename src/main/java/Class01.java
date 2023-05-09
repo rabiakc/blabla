0ebdfe5b394a61e7dd83cd327917a6c1c4b76a50
Rabia Koç <rabiakkcc@gmail.com>
2023-05-09 15:21:06 +0300

    V2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,19 +1,7 @@
 public class Class01 {
     public static void main(String[] args) {
-        int x = 1;
-        int y = 2;
-        System.out.println(x+y);
-        System.out.println("Versiyon 1 için işlemler tamam!");
-        // Versiyon 2 çalışmaları...
-        int z = 4;
-        System.out.println(x+y+z);
-        //V3
-        System.out.println("V3 kodları...");
-        //V4
-        System.out.println("V4çalışmaları");
-        //V5 kodlarım...
-
-
+        //V1 çalışmalarım...
+        //V2 kodları
 
     }
 }
